82a88f7ab349d5d27f6c3bcedea008576a56fce3
MariaMedina <mariawestengen@icloud.com>
2026-05-26 21:22:42 +0200

    starting design

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -6,6 +6,6 @@ router = express.Router();
 
 router.get('/logg-inn', defaultControllers.loginGET);
 router.post('/logg-inn', defaultControllers.loginPOST);
-router.get('/', middleware.authenticate, defaultControllers.problems);
+router.get('/', middleware.authenticate, defaultControllers.index);
 
 module.exports = router;
